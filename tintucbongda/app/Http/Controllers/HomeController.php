a3ce9e70d127559378cfbb334aa0578c913878ee
Zy Khuong <0306221132@caothang.edu.vn>
2025-11-20 13:47:04 +0700

    Cập nhật routes và thêm HomeController

---
+++ tintucbongda/app/Http/Controllers/HomeController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class HomeController extends Controller
+{
+    public function video()
+{
+    return view('video'); // Trả về view video.blade.php
+}
+    public function home()
+{
+    return view('home'); // Trả về view video.blade.php
+}
+}

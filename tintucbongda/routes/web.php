cdac79dc1b6317e67034da6001cbfd517d648948
Duy <luonganhduy1002@gmail.com>
2025-11-21 18:03:58 +0700

    Định dạng lại web.php

--- tintucbongda/routes/web.php
+++ tintucbongda/routes/web.php
@@ -6,12 +6,8 @@ use App\Http\Controllers\Auth\LoginController;
 use App\Http\Controllers\VideoController;
 use App\Http\Controllers\DanhMucController;
 
-
-
 Route::get('/', [HomeController::class, 'home'])->name('home');
 Route::get('/video', [HomeController::class, 'video'])->name('video');
-
-
 
 Route::resource('DanhMuc', DanhMucController::class);
 Route::get('danhmucs/{id}/edit', [DanhMucController::class, 'edit'])->name('danhmucs.edit');
@@ -23,17 +19,9 @@ Route::post('/categories', [DanhMucController::class, 'store'])->name('categorie
 Route::resource('videos', VideoController::class);
 Route::get('/video/{id}', [VideoController::class, 'show'])->name('video.show');
 
-
-
-
-
-
-
 //Login
 Route::get('/login', [LoginController::class, 'showFormLogin'])->name('login.show');
 Route::post('/login', [LoginController::class, 'login'])->name('login.post');
 Route::post('/logout', [LoginController::class, 'logout'])->name('logout');
 Route::get('/register', [LoginController::class, 'showFormRegister'])->name('register.show');
 Route::post('/register', [LoginController::class, 'register'])->name('register.post');
-
-
